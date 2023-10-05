7795a4f17c066fc75dc1ab7c129f49a6bc306e22
aRtuhr10 <1104kevinlnv@gmail.com>
2023-10-05 09:47:24 -0500

    Navbar  de home page finalizada y responsiva

--- Hoteleria_frontend/src/components/Navbar_Home.js
+++ Hoteleria_frontend/src/components/Navbar_Home.js
@@ -1,32 +1,34 @@
-import React from 'react'
+
 import SenaLogo from '../images/LogoSena.png';
+import './Navbar_Home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import './Navbar_Home.css'
-
-const Navbar_Home = () => {
-    return (
-        <nav className="navbar navbar-expand-sm">
-          <a className="navbar-brand">
-            <img src={SenaLogo} width="50" height="50" className="img-fluid" />
-          </a>
-          
-          <div className="navbar-collapse nav-link d-flex align-items-center justify-content-center">
-            <ul className="navbar-nav mx-auto justify-content-center">
-              <li className="nav-item bold">
-                <a className="nav-link" href="#">Regional Tolima</a>
-              </li>
-              <li className="nav-item bold">
-                <a className="nav-link" href="#">Conoce más</a>
-              </li>
-              <li className="nav-item bold">
-                <a className="nav-link" href="#">Hotelería</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      );
-    }
+import 'bootstrap-icons/font/bootstrap-icons.json'
+const Navbar_Home = () =>  {
+  return (
+    
+    <nav class="navbar navbar-expand-lg justify-content-center mx-auto p-2">
+      <div class="container-fluid">
+        <img class='logo' src={SenaLogo} alt='LogoSena' width="50px" height="50px"></img>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+          <ul class="navbar-nav fw-bold">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Regional Tolima</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Conoce Más</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Hotelería</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar_Home
